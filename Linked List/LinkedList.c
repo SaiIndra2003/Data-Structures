d70663cf8d61e798bb0ae96029650a845241c661
SaiIndra2003 <suryasaiindra@gmail.com>
2023-07-08 10:24:06 +0530

    delete changes

--- Linked List/LinkedList.c
+++ Linked List/LinkedList.c
@@ -85,10 +85,13 @@ void Delete(int pos){
         i--;
     }
     if(pos==size){
+        free(temp->next->next);
         temp->next = NULL;
         return;
     }
-    temp->next = temp->next->next;
+    struct Node* temp1 = temp->next;
+    temp->next = temp1->next;
+    free(temp1);
     return;
 
 }
